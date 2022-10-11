3e0f42591c2dc2a9a801b6b132e9f7270b41d761
SamjoeTL <105303014+SamjoeTL@users.noreply.github.com>
2022-10-11 10:05:20 +0800

    Add files via upload

---
+++ IKI Meet 1/Add Object (get hero from myworld) and Move Object (Move Hero)/MyWorld.java
@@ -0,0 +1,30 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (Tegar Samjoe Indra Laka) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+
+    private void prepare()
+    {
+
+        Main_Character main_Character = new Main_Character();
+        addObject(main_Character,50,200);
+        
+    }
+}
